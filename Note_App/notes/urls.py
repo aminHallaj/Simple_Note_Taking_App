77dd4f6dfafcd27a453fc013edd43b60642e18fc
Amin-Hallaj <aminhallajm@gmail.com>
2024-10-18 21:33:42 +0330

    fix the error

--- Note_App/notes/urls.py
+++ Note_App/notes/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.note_list, name='note_list'),
     path('add/', views.add_note, name='add_note'),
-    path('delete/<id>/', views.delete_note, name='delete_note')
+    path('delete/<note_id>/', views.delete_note, name='delete_note')
 ]
