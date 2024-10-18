5bb1086302ff387b3990eae58df29127154830a3
Amin-Hallaj <aminhallajm@gmail.com>
2024-10-18 20:30:17 +0330

    Display contents and add notes

--- Note_App/notes/views.py
+++ Note_App/notes/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Note
+from .forms import NoteForm
 
-# Create your views here.
+
+def note_list(request):
+    notes = Note.objects.all()
+    return render(redirect, 'note_list.html', {'notes': notes})
+
+def add_note(request):
+    if request.method == 'POST':
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('note_list')
+    else:
+        form = NoteForm()
+        return render(request, 'add_note.html', {'form': form})
