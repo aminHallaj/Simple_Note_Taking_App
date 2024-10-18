14c2312a9ca9cb200e36e91605617966ea1efb5d
Amin-Hallaj <aminhallajm@gmail.com>
2024-10-18 21:08:51 +0330

    Display the number of notes and delete operations

--- Note_App/notes/views.py
+++ Note_App/notes/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Note
 from .forms import NoteForm
 
 
 def note_list(request):
     notes = Note.objects.all()
-    return render(request, 'note_list.html', {'notes': notes})
+    note_count = notes.count()
+    return render(request, 'note_list.html', {'notes': notes, 'note_count': note_count})
 
 def add_note(request):
     if request.method == 'POST':
@@ -16,3 +17,8 @@ def add_note(request):
     else:
         form = NoteForm()
         return render(request, 'add_note.html', {'form': form})
+    
+def delete_note(request, note_id):
+    note = get_object_or_404(Note, id=note_id)
+    note.delete()
+    return redirect('note_list')
